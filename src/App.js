341018f0cdf8688e8cb06e7ddaeee38140aab1d0
Aakanksha <aakankshakarambele@gmail.com>
2023-01-20 18:09:47 +0530

    first calculator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+
 function App() {
+  const [user , setuser] = useState("");
+  const [re , setre] = useState("");
+  const data = (e) =>{
+       setuser(user.concat(e.target.value));
+  }
+ 
+  const blank = () =>{
+       setuser("");
+       setre("");
+      
+     };
+     const del = () =>{
+       setuser(user.slice(0, -1));
+       setre("");
+     }
+     const result = () =>{
+       try{ 
+              setre(eval(user));
+             
+       }catch(e){
+              console.log(e);
+       }
+      
+     }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='center'>
+
+    <div>
+<h1>Calculator</h1>
     </div>
+    <div className='box'>
+    
+    <input className="inp1" type="text" placeholder="" value={re}/>  
+             <input className="inp2" type="text" placeholder="0" value={user}/>  
+   
+      
+      <div>
+      <div>  
+       
+       <input type="text" value="C" className='size col' onClick={blank}/>
+      <input type="text" value="DEL" className='size2 col' onClick={del}/>
+      <input type="text" value="+" className='size col' name="+" onClick={data}/>
+       </div>
+      <div>
+      <input type="text" value="1" className='size' onClick={data}/>
+      <input type="text" value="2" className='size' onClick={data}/>
+      <input type="text" value="3" className='size' onClick={data}/>
+      <input type="text" value="-" className='size col' onClick={data}/>
+      
+      </div>
+      <div>
+      <input type="text" value="4" className='size' onClick={data}/>
+      <input type="text" value="5" className='size' onClick={data}/>
+      <input type="text" value="6" className='size' onClick={data}/>
+      <input type="text" value="*" className='size col' name="*" onClick={data}/>
+     
+      </div>
+      
+      <div>
+      <input type="text" value="7" className='size' onClick={data}/>
+      <input type="text" value="8" className='size' onClick={data}/>
+      <input type="text" value="9" className='size' onClick={data}/>
+      <input type="text" value="/" className='size col' name="*" onClick={data}/>
+
+      
+      </div>
+      <div>
+      <input type="text" value="." className='size' onClick={data}/>
+      <input type="text" value="0" className='size' onClick={data}/>
+      <input type="text" value="=" className='size2 col' onClick={result}/>
+     
+             
+      </div>
+      </div>
+
+    </div>
+    </div>
+    </>
   );
 }
 
